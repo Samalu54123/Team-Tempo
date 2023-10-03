d210c58a361644c99276f20d53730f0c07de3b70
Basichy <ethanmarcg@gmail.com>
2023-10-03 22:37:35 +1300

    Added undo button and styled the Track.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
-        Site Name
+        Team Tempo
       </Link>
       <ul>
         <CustomLink to="/Project">Project</CustomLink>
--- src/Pages/Track.js
+++ src/Pages/Track.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Howl, Howler } from 'howler';
-import * as Tone from 'tone';
 import SoundLibrary from '../SoundLibrary';
+import '../Track.css';
 
 class Track extends Component {
   constructor(props) {
@@ -10,6 +10,7 @@ class Track extends Component {
     this.state = {
       timelineClips: [], // Array to store added clips
       playbackPosition: 0, // Keep track of playback position (in seconds)
+      playbackSpeed: 1.0, // Default playback speed
     };
 
     this.soundInstances = [];
@@ -21,21 +22,28 @@ class Track extends Component {
       timelineClips: [...prevState.timelineClips, clipSound],
     }));
   };
-  
+
+  // Handle playback speed change
+  handleSpeedChange = (event) => {
+    const speed = parseFloat(event.target.value);
+    this.setState({ playbackSpeed: speed });
+  };
+
+  // Play the timeline
   playTimeline = () => {
-    const { timelineClips } = this.state;
-  
+    const { timelineClips, playbackSpeed } = this.state;
+
     if (Howler.ctx.state !== 'running') {
       Howler.ctx.resume(); // Resume the audio context if it's not running
     }
-  
+
     // Clear any existing sound instances
     this.soundInstances.forEach((sound) => sound.unload());
     this.soundInstances = [];
-  
+
     // Define the delay interval between clip playback (in milliseconds)
-    const delayInterval = 0;
-  
+    const delayInterval = 200; // 200 milliseconds (0.2 seconds)
+
     // Function to play a clip with a delay
     const playClipWithDelay = (index) => {
       if (index < timelineClips.length) {
@@ -43,6 +51,7 @@ class Track extends Component {
         const sound = new Howl({
           src: [clip.sound],
           volume: 1.0, // Adjust volume as needed
+          rate: playbackSpeed, // Set the playback speed
           onend: () => {
             setTimeout(() => {
               // Play the next clip with the next index after the delay
@@ -50,7 +59,7 @@ class Track extends Component {
             }, delayInterval);
           },
         });
-  
+
         // Add the sound instance to the array
         this.soundInstances.push(sound);
 
@@ -60,26 +69,60 @@ class Track extends Component {
 
     playClipWithDelay(0);
   };
-  
+
+  // Handle the "Undo" action
+  undoLastClip = () => {
+    const { timelineClips } = this.state;
+    if (timelineClips.length > 0) {
+      const updatedTimeline = [...timelineClips];
+      updatedTimeline.pop(); // Remove the last clip
+      this.setState({ timelineClips: updatedTimeline });
+    }
+  };
 
   render() {
     Howler.volume(1.0); // Set the volume for Howler
-    const { timelineClips, playbackPosition } = this.state;
+    const { timelineClips, playbackPosition, playbackSpeed } = this.state;
 
     return (
-      <div>
+      // Add a div with a class name for styling
+      <div className="track-container">
         <h2>Timeline</h2>
-        <button onClick={this.playTimeline}>Play Timeline</button>
-        <p>Playback Position: {playbackPosition.toFixed(2)} seconds</p>
+        <div className="timeline-header">
+          {/* Playback controls */}
+          <div className="playback-controls">
+            <button onClick={this.playTimeline}>Play Timeline</button>
+          </div>
+          <div className="playback-speed">
+            {/* Playback speed dropdown */}
+            <label className="playback-speed-label">Playback Speed:</label>
+            <select
+              className="playback-speed-select"
+              onChange={this.handleSpeedChange}
+              value={playbackSpeed}
+            >
+              <option value={1.0}>Normal</option>
+              <option value={0.5}>Half Speed</option>
+              <option value={1.5}>1.5x Speed</option>
+              <option value={2.0}>Double Speed</option>
+            </select>
+          </div>
+          <div className="undo-button">
+            {/* Undo button */}
+            <button onClick={this.undoLastClip}>Undo</button>
+          </div>
+        </div>
 
         <SoundLibrary addClipToTimeline={this.addClipToTimeline} />
 
-        <h3>Timeline Clips</h3>
-        {timelineClips.map((clip, index) => (
-          <div key={index}>
-            {clip.label}
-          </div>
-        ))}
+        {/* Timeline Clips */}
+        <div className="timeline-clips">
+          {timelineClips.map((clip, index) => (
+            <div key={index} className="timeline-clip">
+              {clip.label}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
